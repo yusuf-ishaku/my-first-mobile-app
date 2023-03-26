b7b80e219e05da8312d3ac4cee279737378755cd
yusuf <kingshakzy@gmail.com>
2023-03-26 15:46:31 -0700

    Started building

--- App.js
+++ App.js
@@ -1,23 +1,20 @@
 import { useReducer, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   const [text, setText] = useState("Fortune's second app!")
   return (
     <View style={styles.container}>
-      <Text>{text}</Text>
-      <Button title='Get Started' onPress={() =>{setText("Fortune's first app")}}/>
-      <StatusBar style="auto" />
+      <View>
+        <TextInput/>
+        <Button title='Add'></Button>
+      </View>
+      <View></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ 
 });

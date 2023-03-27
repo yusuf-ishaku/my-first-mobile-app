888813cd8bd97548993618c58585eba23afd2406
yusuf <kingshakzy@gmail.com>
2023-03-27 10:06:42 -0700

    Stylesheet created

--- App.js
+++ App.js
@@ -5,10 +5,10 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 export default function App() {
   const [text, setText] = useState("Fortune's second app!")
   return (
-    <View style={styles.container}>
-      <View>
-        <TextInput/>
-        <Button title='Add'></Button>
+    <View style={styles.screen}>
+      <View style={styles.inputPart}>
+        <TextInput placeholder='New Task' style={styles.input}/>
+        <Button title='Add' style={{backgroundColor: 'black'}}></Button>
       </View>
       <View></View>
     </View>
@@ -16,5 +16,18 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
- 
-});
+  screen:{
+    padding: 30
+  },
+  inputPart:{
+    flexDirection: "row", 
+    justifyContent: 'space-between', 
+    alignItems: 'center'
+  },
+  input:{
+    borderColor: 'black',
+    padding: 5,
+    borderBottomWidth: 1,
+    width: '80%'
+  }
+})
